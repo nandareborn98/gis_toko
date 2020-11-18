9d3027251dde46b30fbcaef11e11752463426323
nandareborn98 <intanaprilioni99@gmail.com>
2020-11-18 13:16:33 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,5 @@
+<div class="page-header">
+    <h1>Sistem Informasi Geografis</h1>
+</div>
+<p>sistem informasi geografis ini dibuat untuk mempermudah pelanggan dalam mencari toko ikan hias yang berada di kota jambi </p>
+	
